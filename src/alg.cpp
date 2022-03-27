325079ddc09b1f936494f51937ed038a47074446
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-03-27 16:55:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,5 @@
 // Copyright 2021 NNTU-CS
-/*void bubbleSort(int *arr, int len) {
+void bubbleSort(int *arr, int len) {
   int zam = 0;
   for (int i = 0; i < len; i++) {
     for (int j = len - 1; j >= i + 1; j--) {
@@ -10,10 +10,10 @@
       }
     }
   }
-}*/
+}
 
 int countPairs1(int *arr, int len, int value) {
-  //bubbleSort(arr, len);
+  bubbleSort(arr, len);
   int k = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = (i + 1); j < len; j++) {
@@ -26,7 +26,7 @@ int countPairs1(int *arr, int len, int value) {
 }
 
 int countPairs2(int *arr, int len, int value) {
-  //bubbleSort(arr, len);
+  bubbleSort(arr, len);
   int k = 0;
   while (arr[len - 1] > value) {
     len--;
@@ -41,7 +41,7 @@ int countPairs2(int *arr, int len, int value) {
   return k;
 }
 
-int cbinsearch(int *arr, int size, int value, int ii) {
+/*int cbinsearch(int *arr, int size, int value, int ii) {
   int count = 0;
   int i = ii, j = size - 1;
   while (i <= j) {
@@ -64,13 +64,26 @@ int cbinsearch(int *arr, int size, int value, int ii) {
       }
   }
   return count;
-}
+}*/
 
 int countPairs3(int *arr, int len, int value) {
-  //bubbleSort(arr, len);
+  bubbleSort(arr, len);
   int k = 0;
-  for (int i = 0; i < len; i++) {
-    k += cbinsearch(arr, len, value - arr[i], i + 1);
-  }
+    for (int i = 0; i < len; i++) {
+        int ii = i + 1;
+        int jj = len - 1;
+        while (ii < jj) {
+            int mid = (ii + jj) / 2;
+            if (arr[mid] + arr[i] < value) {
+                ii = mid + 1;
+            } else {
+                jj = mid;
+            }
+        }
+        while (arr[ii] + arr[i] == value) {
+            k += 1;
+            ii += 1;
+        }
+    }
   return k;
 }
