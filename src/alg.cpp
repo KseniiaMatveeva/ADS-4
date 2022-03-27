80610e434f49e89ef17b53e3d080a0ac9725f89d
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-03-27 15:38:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -28,10 +28,10 @@ int countPairs1(int *arr, int len, int value) {
 int countPairs2(int *arr, int len, int value) {
   bubbleSort(arr, len);
   int k = 0;
-  /*while (arr[len - 1] > value) {
+  while (arr[len - 1] > value) {
     len--;
-  }*/
-  for (int i = 0; i < len; i++) {
+  }
+  for (int i = 0; i < len - 1; i++) {
     for (int j = len - 1; j > i; j--) {
       if (arr[i] + arr[j] == value) {
         k++;
@@ -44,21 +44,21 @@ int countPairs2(int *arr, int len, int value) {
 int cbinsearch(int *arr, int size, int value, int ii) {
   int count = 0;
   int i = ii, j = size - 1;
-  while (i < j) {
+  while (i <= j) {
     int mid = i + (j - i) / 2;
-    if (arr[mid] == value && mid != ii) {
+    if (arr[mid] == value && mid != ii && mid != size) {
       count++;
-      while (arr[mid - 1] == value) {
+      while (arr[mid - 1] == value && mid > ii) {
           count++;
           mid--;
         }
-      while (arr[mid + 1] == value) {
+      while (arr[mid + 1] == value && mid < len) {
           count++;
           mid++;
         }
-      return count;
+      break;
      } else if (arr[mid] > value) {
-       j = mid;
+       j = mid - 1;
      } else {
        i = mid + 1;
       }
