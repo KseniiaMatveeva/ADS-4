5001c21494a31860b26fe8e7468db3ba324c0e66
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-03-27 13:06:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,8 +2,8 @@
 int countPairs1(int *arr, int len, int value) {
   int k = 0;
   int i = 0;
-  while (i < len){
-    for (int j = i + 1; j < len; j++){
+  while (i < len) {
+    for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
         k++;
       }
@@ -24,8 +24,8 @@ int countPairs2(int *arr, int len, int value) {
     }
     i++;
     j--;
-  }
-  return k;*/
+  }*/
+  return 0;
 }
 int countPairs3(int *arr, int len, int value) {
   /*int k = 0;
@@ -46,6 +46,6 @@ int countPairs3(int *arr, int len, int value) {
        }
     }
     i++;
-  }
-  return k;*/
+  }*/
+  return 0;
 }
