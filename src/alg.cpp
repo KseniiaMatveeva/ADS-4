776eddd02b4891a354dbeaf343114c4e764e8c7c
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-03-27 13:35:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,10 +2,10 @@
 int countPairs1(int *arr, int len, int value) {
   int k = 0;
   int i = 0;
-  while (i < len) {
+  while (i < len - 1) {
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
-        k++;
+        k++ ;
       }
     }
     i++;
@@ -15,7 +15,7 @@ int countPairs1(int *arr, int len, int value) {
 int countPairs2(int *arr, int len, int value) {
   int k = 0;
   int i = 0;
-  while (i < len) {
+  while (i < len - 1) {
     for (int j = len - 1; j > i; j--) {
       if (arr[i] + arr[j] == value) {
         k++;
@@ -29,13 +29,21 @@ int countPairs3(int *arr, int len, int value) {
   int k = 0;
   int i = 0;
   int j = len - 1;
-  while (arr[i] < (value / 2)) {
+  while (i < len ) {
     int per = value - arr[i];
     int ii = i + 1;
     while (ii < j) {
-      int mid = ii + (j - ii) / 2;
+      int mid = (ii + j) / 2;
       if (arr[mid] == per) {
         k++;
+        while (arr[mid - 1] == per) {
+          k++;
+          mid--;
+        }
+        while (arr[mid + 1] == per) {
+          k++;
+          mid++;
+        }
         break;
        } else if (arr[mid] > per) {
          j = mid;
