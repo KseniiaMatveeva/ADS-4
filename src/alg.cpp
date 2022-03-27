0d66ef889ec18a8747543f9cc0e6ba8ad4f187a2
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-03-27 15:27:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,7 +15,7 @@ void bubbleSort(int *arr, int len) {
 int countPairs1(int *arr, int len, int value) {
   bubbleSort(arr, len);
   int k = 0;
-  for (int i = 0; i < len; i++) {
+  for (int i = 0; i < len - 1; i++) {
     for (int j = (i + 1); j < len; j++) {
       if (arr[i] + arr[j] == value) {
         k++;
